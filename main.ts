52e27b909b41452c30a8ba51d6726d871b48248d
aesher <aashis.kumar@quillbot.com>
2023-05-06 16:24:25 +0530

    Added hand;ers

--- main.ts
+++ main.ts
@@ -1,6 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import grpc from '@grpc/grpc-js'
 import protoLoader from '@grpc/proto-loader'
 import { ProtoGrpcType } from './types/proto/externalscaler'
+import { ExternalScalerHandlers } from './types/proto/externalscaler/ExternalScaler'
+
+const GlobalPodAutoscaler: ExternalScalerHandlers = {
+  IsActive: null as any,
+  GetMetrics: null as any,
+  GetMetricSpec: null as any,
+  StreamIsActive: null as any
+}
 
 const packageDefinition = protoLoader.loadSync('./proto/externalscaler.proto', {
   keepCase: true,
@@ -9,4 +18,7 @@ const packageDefinition = protoLoader.loadSync('./proto/externalscaler.proto', {
   defaults: true,
   oneofs: true
 })
-const packageObject = grpc.loadPackageDefinition(packageDefinition) as unknown as ProtoGrpcType
+const proto = grpc.loadPackageDefinition(packageDefinition) as unknown as ProtoGrpcType
+
+const server = new grpc.Server()
+server.addService(proto.externalscaler.ExternalScaler.service, GlobalPodAutoscaler)
